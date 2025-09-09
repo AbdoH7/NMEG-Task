61fd5a282d3007d7bd76cf00736b0df3d0ccfa52
Abdulrahman Hussein <abdulrahman.hussein@nm-eg.com>
2025-09-09 12:58:53 +0300

    Initial commit

---
+++ nmeg-task-backend/src/main/java/org/example/nmegtaskbackend/NmegTaskBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.nmegtaskbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmegTaskBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NmegTaskBackendApplication.class, args);
+    }
+
+}
