721d9fa6c41d7483aaaf4639ee844e8f57b8bfb8
AbdoH7 <abdo.ah1102@gmail.com>
2025-09-14 04:28:50 +0300

    Configure apollo client

---
+++ nmeg-task-frontend/src/apollo/client.ts
@@ -0,0 +1,28 @@
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+
+const httpLink = createHttpLink({
+  uri: import.meta.env.VITE_GRAPHQL_ENDPOINT || 'http://nmeg-task-backend:8080/graphql',
+})
+
+const authLink = setContext((_: any, { headers }: { headers: any }) => {
+  return {
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json',
+    }
+  }
+})
+
+export const apolloClient = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      errorPolicy: 'all',
+    },
+    query: {
+      errorPolicy: 'all',
+    },
+  },
+})
--- nmeg-task-frontend/src/main.ts
+++ nmeg-task-frontend/src/main.ts
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
+import { DefaultApolloClient } from '@vue/apollo-composable'
 import App from './App.vue'
 import router from './router'
+import { apolloClient } from './apollo'
+import './style.css'
 
 const app = createApp(App)
 
 app.use(router)
+app.provide(DefaultApolloClient, apolloClient)
 
 app.mount('#app')
